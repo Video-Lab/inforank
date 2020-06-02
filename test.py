d6996bc617cae4c9364805cf8a223aed54365ac8
Jad Khalili <jad@videolab.ae>
2020-06-02 16:42:24 -0500

    Fix testing

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 from importlib import import_module
+from sys import modules
 
 
 # Testing info
 TESTS = [["parseFileTest", {'path': './tests/testfile.txt'}]]
 
 def parseFileTest(path):
-	video = parseFile(path)
-	print(dir(video))
-	
-if __name__ == "__main__":
 	import fileparser
+	video = fileparser.parseFile(path)
+	print(video.title)
+	print(video.width)
+	print(video.height)
+	print(video.data_boxes[0].data_box_width)
+	print(video.data_boxes[0].data_value)
+	print(video.data_boxes[0].data_title)
+
+if __name__ == "__main__":
 
 	for test in TESTS:
-		print(getattr(fileparser, test[0])(**test[1]))
+		getattr(modules[__name__], test[0])(**test[1])
