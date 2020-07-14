fd436c822983ab60466b168c15132c69f83c6484
Jad Khalili <jad@videolab.ae>
2020-07-14 00:09:22 -0500

    Comments & cleanup

--- databox.py
+++ databox.py
@@ -70,7 +70,7 @@ class DataBox:
 		title_coords = self.getDataTitleCoordinates()
 		return [(0,title_coords[1][1]),(self.data_box_width,int(title_coords[1][1]+(DATA_IMAGE_PERCENTAGE*self.data_box_height)))]
 
-	def getSafeDataImageCoordinates(self):
+	def getSafeDataImageCoordinates(self): # Get area for image to be placed in box
 		image_coords = self.getDataImageCoordinates()
 		x_pad = int( (DATA_IMAGE_PADDING_PERCENTAGE*(image_coords[1][0]-image_coords[0][0]))/2 )
 		y_pad = int( (DATA_IMAGE_PADDING_PERCENTAGE*(image_coords[1][1]-image_coords[0][1]))/2 )
@@ -99,19 +99,23 @@ class DataBox:
 				break
 		return font
 
-	def writeText(self, img, text, font, color, coords): # Writes the text with the given font and coordinates, making sure to horizontally center.
+	def writeText(self, img, text, font, color, coords):
+		# Writes the text with the given font and coordinates, making sure to horizontally center.
 
+		#Set up draw ctx, font
 		draw = ImageDraw.Draw(img, mode='RGB')
 		text_size = font.getsize(text)
+
+		#Calculate boxes, coordinates, horizontal centering
 		w,h = [coords[1][0]-coords[0][0], coords[1][1]-coords[0][1]] # width and height
 		left_pad = (w-text_size[0])/2
 		corner = [coords[0][0]+left_pad-font.getoffset(text)[0], coords[0][1]-font.getoffset(text)[1]]
-		draw.text(corner, text, font=font, fill=color)
-		# draw.rectangle([tuple(corner), (corner[0]+text_size[0], corner[1]+text_size[1])], outline='black') # Border around text
-		return img
 
-	def getImage(self): # Gets the image from the file or icon API, error checking included
-		pass
+		draw.text(corner, text, font=font, fill=color)
+
+		# Border around text (debug only)
+		# draw.rectangle([tuple(corner), (corner[0]+text_size[0], corner[1]+text_size[1])], outline='black')
+		return img
 
 	def writeDataValue(self, img): # Writes data value text (specific call to writeText)
 		inner_coords = self.getInnerDataValueCoordinates()
@@ -125,7 +129,7 @@ class DataBox:
 
 		# Fit text to boxes (reduce size if needed)
 
-		# # Calculating coords for text boxes
+		# Calculating coords for text boxes
 		prefix_size = np.subtract(prefix_font.getsize(self.prefix), prefix_font.getoffset(self.prefix))
 		main_size = np.subtract(main_font.getsize(self.data_value), main_font.getoffset(self.data_value))
 		suffix_size = np.subtract(suffix_font.getsize(self.suffix), suffix_font.getoffset(self.suffix))
@@ -138,13 +142,12 @@ class DataBox:
 
 		suffix_coords = [ (main_coords[0][0], main_coords[1][1]+TEXT_PADDING) , (main_coords[1][0], main_coords[1][1]+TEXT_PADDING+suffix_size[1]) ]
 
-		# #Drawing text
+		#Drawing text
 		img = self.writeText(img, self.prefix, prefix_font, text_color, prefix_coords)
 		img = self.writeText(img, self.data_value, main_font, text_color, main_coords)
 		img = self.writeText(img, self.suffix, suffix_font, text_color, suffix_coords)
 
 		# Draw borders around text areas (debug only)
-
 		# draw = ImageDraw.Draw(img, mode='RGB')
 		
 		# draw.rectangle(prefix_coords, outline='white')
@@ -153,42 +156,65 @@ class DataBox:
 
 		return img
 
-
-
-	def writeDataTitle(self, img): # ...
+	def writeDataTitle(self, img): 
+		
+		# Gets title colors and size
 		text_color = getGoodTextColor(self.bg_light_color)
 		title_coords = self.getDataTitleCoordinates()
 		title_font = self.generateTextFont(self.data_title,size=DATA_TITLE_FONT,bold=True,box=title_coords)
+
+		# Adjusts box based on font offset & centers title
 		title_size = np.subtract(title_font.getsize(self.data_title), title_font.getoffset(self.data_title))
 		top_pad = ((title_coords[1][1] - title_coords[0][1]) - title_size[1]) / 2
-	
+
+		# Gets new title co-ordinates & writes text from internal functions
 		title_coords_new = [(title_coords[0][0], title_coords[0][1]+top_pad) , (title_coords[1][0], title_coords[1][1])]
 		img = self.writeText(img, self.data_title,title_font,text_color,title_coords_new)
 
 		return img
 
-
 	def getDataImage(self):
+		# Checks if data type is loading from file
 		if self.data_image_type == "file":
+			# Open from given path and convert to RGBA for transparency
 			img = Image.open(os.path.abspath(self.data_image)).convert("RGBA")
 		else:
+			# Call to function that gets icon
 			img = self.getDataIcon()
 		return img
 
 	def getDataIcon(self):
-		url = "https://api.iconfinder.com/v4/icons/search?query=" + self.data_image + "&count=10&vector=0&premium=0" # Don't include vector icons, max 10, query from data image value
+		# Don't include vector icons, max 10, query from data image value
+		url = "https://api.iconfinder.com/v4/icons/search?query=" + self.data_image + "&count=10&vector=0&premium=0"
+
+		# Default value as fallback in case of API error
 		img_url = os.path.abspath("./assets/none.png")
+
+		# Declaration to be used for later
 		img = None
+
+		# Auth from environment variable
 		headers = {'Authorization': 'Bearer ' + ICONFINDER_API_KEY}
+
 		resp = requests.get(url, headers=headers)
+
+		# Checks if too many requests (HTTP Code 429)
 		while resp.status_code == 429:
+
+			# Tries request every second until timeout ends
 			print("Too many requests to IconFinder API. Waiting 1 second...")
 			time.sleep(1)
 			resp = requests.get(url, headers=headers)
+
+		# Other than OK code
 		if resp.status_code != 200:
+
+			# Loads image from fallback URL
 			print("Error in connection with error code " + resp.status_code + ", defaulting to none.png")
 			img = Image.open(img_url).convert("RGBA")
+		
 		else:
+			#Convert to JSON, pull URL to image from largest size raster images, stores all image URLs for preview later
 			resp = resp.json()
 			for icon in resp['icons']:
 				biggest = len(icon['raster_sizes'])-1
@@ -199,9 +225,11 @@ class DataBox:
 		return img
 
 	def getDataIconFromURL(self, url):
+		# Loads image from external URL
 		return Image.open(BytesIO(requests.get(url).content))
 
 	def writeDataImage(self, img, data_img):
+		# Gets the image and image coordinates, other calculations and declarations
 		data_img = self.getDataImage()
 		image_coords = self.getSafeDataImageCoordinates()
 		w,h = image_coords[1][0] - image_coords[0][0], image_coords[1][1]-image_coords[0][1]
@@ -209,11 +237,14 @@ class DataBox:
 		ratio = newsize[1] / newsize[0]
 		bigger = None
 
+		# Checks if either width or height is bigger than given width/height. If both, will resize based on height.
 		if data_img.size[0] > w:
 			bigger = "w"
 		if data_img.size[1] > h:
 			bigger = "h"
 		
+
+		#Resizes image based on oversized axis from previous conditional, adjusts opposite axis based on aspect ratio
 		if bigger == "w":
 			newsize[0] = int(IMAGE_PERCENTAGE[0]*w)
 			newsize[1] = int(ratio*newsize[0])
@@ -221,21 +252,25 @@ class DataBox:
 			newsize[1] = int(IMAGE_PERCENTAGE[1]*h)
 			newsize[0] = int((1/ratio)*newsize[1])
 
-		# data_img = data_img.resize(newsize)
+		# Calculates padding to center image in safe box
 		left_pad = int((w-newsize[0])/2)
 		top_pad = int((h-newsize[1])/2)
-
+		
+		#Resize & overlay image
 		data_img = data_img.resize(newsize)
 		img.paste(data_img, [image_coords[0][0]+left_pad, image_coords[0][1]+top_pad], mask=data_img)
+
 		return img
 
 
-	def generateImage(self): # Generates the image as a whole
+	def generateImage(self):
+		# Generates the image as a whole
 		self.image = self.generateImageBase()
 		self.image = self.writeDataValue(self.image)
 		self.image = self.writeDataTitle(self.image)
 		self.image = self.writeDataImage(self.image, self.getDataImage())
 		return self.image
 
-	def outputImage(self, out_path): # Writes image to file at out_path
+	def outputImage(self, out_path):
+		# Writes image to file at out_path
 		self.image.save(os.path.abspath(out_path))
